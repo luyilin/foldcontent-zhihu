d4bd6367f0dd47c233de051a12ce6b2a17249b7d
luyilin <luyilin12@gmail.com>
2016-11-22 17:30:35 +0800

    save

--- js/demo.js
+++ js/demo.js
@@ -2,7 +2,7 @@ define(['jquery'], function() {
     (function () {
         var doc = $(document);
         var win = $(window);
-        // 多次使用, 缓存
+        // 多次使用, 缓存起来
         doc.on('click', '.unfold', function () {
             var unfold = $(this);
             if (unfold.text() !== '收起') {
@@ -14,7 +14,7 @@ define(['jquery'], function() {
                     var scrollHeight = doc.scrollTop() + win.height();
                     var right = win.width() / 2 - 350 + 20 > 0 ? win.width() / 2 - 350 + 20 : 20;
                     if (unfold.offset().top - scrollHeight > -50 &&
-                        panel.offset().top - scrollHeight < -90 && unfold.text() === '收起') {
+                        panel.offset().top - scrollHeight < -90 && unfold.text() !== '展开') {
                         unfold.next().show().css('right', right);
                     } else {
                         unfold.next().hide();
