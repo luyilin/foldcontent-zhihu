6faecc33d869b59b53b099a3ae047152825fcae8
luyilin <luyilin12@gmail.com>
2016-11-23 15:10:16 +0800

    scroll事件性能优化,解决同时出现绝对定位和固定定位的两个收起按钮的问题

--- js/demo.js
+++ js/demo.js
@@ -7,30 +7,43 @@ define(['jquery'], function() {
             var unfold = $(this);
             if (unfold.text() !== '收起') {
                 unfold.text('收起').siblings('.part-content').hide().siblings('.all-content').show();
-                win.on('scroll', function () {
-                    // document 对象是 window 对象的一个属性, 是显示于窗口内的一个文档,可以用 window.document 属性访问
-                    // window 是一个顶层对象, 是浏览器中打开的窗口
-                    var panel = unfold.parent();
-                    var scrollHeight = doc.scrollTop() + win.height();
-                    var right = win.width() / 2 - 350 + 20 > 0 ? win.width() / 2 - 350 + 20 : 20;
-                    if (unfold.offset().top - scrollHeight > -50 &&
-                        panel.offset().top - scrollHeight < -90 && unfold.text() !== '展开') {
-                        unfold.next().show().css('right', right);
-                    } else {
-                        unfold.next().hide();
+                var panel = unfold.parent();
+                var right = win.width() / 2 - 350 + 20 > 0 ? win.width() / 2 - 350 + 20 : 20;
+                var panelScroll = panel.offset().top + panel.height();
+                // scroll 事件性能优化
+                // 鼠标滚动时 scroll 事件触发的间隔大约为 10~20 ms, 相对于其他的鼠标、键盘事件,它被触发的频率很高,间隔很近。
+                // 如果 scroll 事件涉及大量的位置计算、元素重绘等工作,且这些工作无法在下个 scroll 事件触发前完成,就会导致浏览器掉帧
+                // 1. 因此需要减少绑定给 scroll 中具体想要执行的业务逻辑的执行次数
+                // 2. 并将对象初始化、不变的高度值等缓存在 scroll 事件外部
+                // 存在 bug : 当以很快的速度滚动时,有可能执行不到 scroll 绑定的事件
+                var cb = {
+                    onscroll: function() {
+                        var scrollHeight = doc.scrollTop() + win.height();
+                        if (scrollHeight - panelScroll < 50 &&
+                            panel.offset().top - scrollHeight < -90 && unfold.text() !== '展开') {
+                            unfold.addClass('fold-fix');
+                            unfold.css('right', right);
+                        } else {
+                            changeStyle(unfold);
+                        }
+                        win.off("scroll", cb.onscroll);
+                        setTimeout(function() {
+                            win.on("scroll", cb.onscroll);
+                        }, 50);
                     }
-                })
+                };
+                win.on("scroll", cb.onscroll);
             } else {
-                $(this).text('展开').siblings('.fold-fix').hide().siblings('.part-content').show()
+                var fold = $(this);
+                changeStyle(fold);
+                fold.text('展开').siblings('.part-content').show()
                     .siblings('.all-content').hide();
             }
         });
 
-        doc.on('click', '.fold-fix', foldFixContent);
-
-        function foldFixContent() {
-            $(this).hide().siblings('.part-content').show()
-                .siblings('.all-content').hide().siblings('.unfold').text('展开');
+        function changeStyle(a) {
+            a.removeClass('fold-fix');
+            a.css('right', '20px');
         }
 
     })()
